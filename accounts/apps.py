02c4ce049f8a06efde4a1fa505cca5d32b4e0835
businessanalystdm <businessanalyst@daymart.in>
2025-07-12 15:40:43 +0530

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -11,8 +11,8 @@ class AccountsConfig(AppConfig):
         def create_admin_user(sender, **kwargs):
             User = get_user_model()
             username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-            email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-            password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin1234')
+            email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@gmail.com')
+            password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Admin@Daymart')
 
             if not User.objects.filter(username=username).exists():
                 print(f"Creating superuser: {username}")
