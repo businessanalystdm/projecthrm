8179cb708600a1fb3254a6a5337469d7b8e8fd12
businessanalystdm <businessanalyst@daymart.in>
2025-07-12 15:39:42 +0530

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
+from django.contrib.auth import get_user_model
+import os
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        def create_admin_user(sender, **kwargs):
+            User = get_user_model()
+            username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+            email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+            password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin1234')
+
+            if not User.objects.filter(username=username).exists():
+                print(f"Creating superuser: {username}")
+                User.objects.create_superuser(username=username, email=email, password=password)
+
+        post_migrate.connect(create_admin_user, sender=self)
